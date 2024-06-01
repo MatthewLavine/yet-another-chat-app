772a2bdae55ca26e666e43028f083bf9f08e85f3
Matthew Lavine <matt@matthewlavine.net>
2024-06-01 19:51:29 -0400

    Room and user counts

--- app/ui/room-list.jsx
+++ app/ui/room-list.jsx
@@ -3,7 +3,7 @@
 export default function RoomList({ rooms }) {
   return (
     <div className="mr-5 min-h-72 rounded-lg bg-slate-600 p-5 text-slate-100">
-      <h2 className="text-lg">Rooms</h2>
+      <h2 className="text-lg">Rooms ({rooms.length})</h2>
       {rooms.map((room, index) => (
         <div key={index}>
           <span>{room.name}</span>
