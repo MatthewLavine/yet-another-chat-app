4293dfd57f32ebff66bcbe17ccdca2d7e596ae23
Matthew Lavine <matt@matthewlavine.net>
2024-06-01 13:28:03 -0400

    Input border

--- app/ui/input-form.jsx
+++ app/ui/input-form.jsx
@@ -31,7 +31,7 @@ export default function InputForm({ socket, addMessage }) {
                 placeholder="Type a message..."
                 autoFocus
                 autoComplete="off"
-                className="grow rounded-lg p-2" />
+                className="grow rounded-lg p-2 border border-blue-500" />
             <button
                 type="submit"
                 className="flex h-10 items-center rounded-lg bg-blue-500 px-4 text-sm font-medium text-white transition-colors hover:bg-blue-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-500 active:bg-blue-600 aria-disabled:cursor-not-allowed aria-disabled:opacity-50">Send</button>
