d552bbbcc6e87ead2080c5d1b2d9feb5451a9bf5
Matthew Lavine <matt@matthewlavine.net>
2024-06-02 19:10:46 -0400

    Basic nonfunctional settings pages

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -1,7 +1,33 @@
-export default async function Page() {
+"use client";
+
+import { useState } from "react";
+
+export default function Page() {
+  const [username, setUsername] = useState("");
+
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    console.log("username", username);
+  }
+
   return (
     <>
       <p>Settings</p>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Pick a username:
+          <input
+            type="text"
+            name="username"
+            placeholder="Username"
+            defaultValue={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </label>
+        <button type="submit" className="rounded-lg bg-green-500 p-2">
+          Save
+        </button>
+      </form>
     </>
   );
 }
