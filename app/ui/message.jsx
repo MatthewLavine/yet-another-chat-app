d08c9765ba804b5cbbd6c920b456f57ef3996ace
Matthew Lavine <matt@matthewlavine.net>
2024-06-29 18:41:07 -0400

    Message styles

--- app/ui/message.jsx
+++ app/ui/message.jsx
@@ -1,9 +1,15 @@
 export default function Message({ message }) {
   return (
-    <div>
-      <span>[{message.time}]&nbsp;</span>
-      <span>&lt;{message.sender}&gt;&nbsp;</span>
-      <span>{message.content}</span>
+    <div class="hyphens-auto text-wrap break-normal">
+      <span className="text-stone-600 dark:text-stone-400">
+        [{message.time}]&nbsp;
+      </span>
+      <span className="text-green-700 dark:text-green-400">
+        &lt;{message.sender}&gt;
+      </span>
+      <span className="text-slate-950 dark:text-slate-50">
+        {message.content}
+      </span>
     </div>
   );
 }
