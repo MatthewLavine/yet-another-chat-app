17cc379eefed598347d82a7961eb45c3c5dce3a9
Matthew Lavine <matt@matthewlavine.net>
2024-06-30 09:38:53 -0400

    Class typo

--- app/ui/date-separator.jsx
+++ app/ui/date-separator.jsx
@@ -2,11 +2,11 @@ export default function DateSeparator({ date }) {
   return (
     <>
       <div className="flex flex-row">
-        <hr class="my-2 mr-2 h-px flex-grow border-0 bg-gray-300 dark:bg-gray-700" />
+        <hr className="my-2 mr-2 h-px flex-grow border-0 bg-gray-300 dark:bg-gray-700" />
         <div className="cursor-default text-center text-xs text-stone-600 dark:text-stone-400">
           {date}
         </div>
-        <hr class="my-2 ml-2 h-px flex-grow border-0 bg-gray-300 dark:bg-gray-700" />
+        <hr className="my-2 ml-2 h-px flex-grow border-0 bg-gray-300 dark:bg-gray-700" />
       </div>
     </>
   );
