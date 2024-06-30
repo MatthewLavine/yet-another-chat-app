4e3093dbe7569a8689730c0f0a61c6483f31ec92
Matthew Lavine <matt@matthewlavine.net>
2024-06-30 10:23:42 -0400

    Remove debug sleep

--- server/index.js
+++ server/index.js
@@ -229,7 +229,7 @@ app.get("/", (req, res) => {
 });
 
 app.get("/api/rooms", async (req, res) => {
-  await new Promise((resolve) => setTimeout(resolve, 500));
+  // await new Promise((resolve) => setTimeout(resolve, 500));
   res.json(rooms.map((room) => room.name));
 });
 
