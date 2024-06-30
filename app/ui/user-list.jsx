549426169f46642fe98065a103562ced6c0f8646
Matthew Lavine <matt@matthewlavine.net>
2024-06-30 09:28:06 -0400

    Userlist shrink fix

--- app/ui/user-list.jsx
+++ app/ui/user-list.jsx
@@ -17,7 +17,7 @@ export default function UserList({ users, connected }) {
   }
 
   return (
-    <div className="hidden w-40 flex-col bg-slate-100 p-5 md:flex dark:bg-slate-700 dark:text-white">
+    <div className="hidden w-40 shrink-0 flex-col bg-slate-100 p-5 dark:bg-slate-700 dark:text-white md:flex">
       <h2 className="text-lg">Users ({users.length})</h2>
       <div className="flex-grow">{output}</div>
     </div>
